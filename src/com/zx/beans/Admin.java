226a31da4fc5df81977fd7bad6ebc19413f8c770
王川川 <1464236582@qq.com>
2022-03-12 22:23:57 +0800

    github测试

--- src/com/zx/beans/Admin.java
+++ src/com/zx/beans/Admin.java
@@ -2,10 +2,18 @@ package com.zx.beans;
 
 public class Admin {
     private int id;
-    private String adminname;
+    private String adminname;//github测试
     private String adminpassword;
     private String adminphone;
     private int state;
+
+
+
+
+
+
+
+
 
 
 
