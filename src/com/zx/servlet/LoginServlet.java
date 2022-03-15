7dd07ca393ab4c72dcaa55ceb6974e8fddf911ba
zl007 <1044836856@qq.com>
2022-03-15 09:11:46 +0800

    删除多余

--- src/com/zx/servlet/LoginServlet.java
+++ src/com/zx/servlet/LoginServlet.java
@@ -12,12 +12,11 @@ import com.zx.dao.impl.StudentDaoImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.Writer;
+
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/LoginServlet")
 public class LoginServlet extends BaseServlet{
