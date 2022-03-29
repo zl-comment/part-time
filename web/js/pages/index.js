c5ec0fed9e2a5cc7001bf56fd472cefb46db82df
zl007 <1044836856@qq.com>
2022-03-29 21:59:55 +0800

    想要修补tab标志卡重复出现的bug失败

--- web/js/pages/index.js
+++ web/js/pages/index.js
@@ -6,11 +6,11 @@ layui.config({
 layui.use(['modules_index'],function(){
 	var modules_index=layui.modules_index
 	,element=layui.element;
+
 	//监听左侧导航点击
   	element.on('nav(nav_left)', function(elem){
 	    var url=elem.attr("href");
 	    //var url=elem.attr("src");
-
 	    console.log(url);
 	    var id=elem.attr("data-id");
 	    console.log(id)
