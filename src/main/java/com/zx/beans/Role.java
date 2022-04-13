94032963f1bf139a97a79c50460fdac371d3b483
zl007 <1044836856@qq.com>
2022-04-13 20:06:57 +0800

    修改了拦截 不知道有没有用

--- src/main/java/com/zx/beans/Role.java
+++ src/main/java/com/zx/beans/Role.java
@@ -7,5 +7,36 @@ public class Role {
     public String rname;
     List<Permission> permissions;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", rname='" + rname + '\'' +
+                ", permissions=" + permissions +
+                '}';
+    }
 }
