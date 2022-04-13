2facd651c2cb478782030b7e054cc57597fe1800
zl007 <1044836856@qq.com>
2022-04-13 20:07:28 +0800

    增加了set/get/toString

--- src/main/java/com/zx/beans/Permission.java
+++ src/main/java/com/zx/beans/Permission.java
@@ -5,6 +5,36 @@ public class Permission {
     public String permname;
     public String permurl;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getPermname() {
+        return permname;
+    }
+
+    public void setPermname(String permname) {
+        this.permname = permname;
+    }
+
+    public String getPermurl() {
+        return permurl;
+    }
+
+    public void setPermurl(String permurl) {
+        this.permurl = permurl;
+    }
+
+    @Override
+    public String toString() {
+        return "Permission{" +
+                "id=" + id +
+                ", permname='" + permname + '\'' +
+                ", permurl='" + permurl + '\'' +
+                '}';
+    }
 }
